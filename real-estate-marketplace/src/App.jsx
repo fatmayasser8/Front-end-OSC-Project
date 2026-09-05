0ce841eaa0fe78dcb1a6115112cdc4995ebab205
fatma <fatma13p0@gmail.com>
2026-09-06 00:42:27 +0300

    .

--- real-estate-marketplace/src/App.jsx
+++ real-estate-marketplace/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<landing />} />
 
 
         <Route path="/auth" element={<Auth />}>
