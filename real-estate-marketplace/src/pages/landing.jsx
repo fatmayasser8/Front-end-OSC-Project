930e1bf869fd3bcc36a7fcde7bb73f9736ca6aa8
fatma <fatma13p0@gmail.com>
2026-09-06 02:09:00 +0300

    Update landing.jsx

--- real-estate-marketplace/src/pages/landing.jsx
+++ real-estate-marketplace/src/pages/landing.jsx
@@ -1,10 +1,8 @@
 import { NavLink } from "react-router-dom";
 import "../styles/landing.css";
-import logo from "../assets/withLogo.png";
 function Landing(){
     return(
         <div className="landing-container">
-            <img src={logo} alt="" />
              <h1>NOVA ESTATES</h1>
 
              <h3>PREMIER LIVING DESTINATIONS</h3>
@@ -12,7 +10,7 @@ function Landing(){
             <p>Welcome to the Art of Living</p>
 
             <p>Explore an exclusive portfolio of homes designed for luxury and tranquility.</p>
-            <NavLink to="/login">
+            <NavLink to="/auth">
             <button>EXPLORING NOW</button>
             </NavLink> 
 
