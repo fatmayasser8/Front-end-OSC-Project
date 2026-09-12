87911cbbdaee1bbdb4264901844b4395c4f49b82
fatma <fatma13p0@gmail.com>
2026-09-12 21:00:02 +0300

    .

--- real-estate-marketplace/src/utils/apiFetch.js
+++ real-estate-marketplace/src/utils/apiFetch.js
@@ -1,32 +1,36 @@
+// apiFetch.js 
 import { refreshAccessToken } from "./refreshToken";
 
 export const apiFetch = async (url, options = {}) => {
   let accessToken = localStorage.getItem("accessToken");
+  
 
-  const makeRequest = async (token) => {
+const makeRequest = async (token) => {
     const isFormData = options.body instanceof FormData;
 
     return fetch(url, {
       ...options,
       headers: {
         ...options.headers,
-        ...(token ? { Authorization: `Bearer ${token}` } : {}),
+        Authorization: `Bearer ${token}`,
         ...(isFormData ? {} : { "Content-Type": "application/json" }),
       },
     });
   };
 
   let response = await makeRequest(accessToken);
+
   if (response.status === 401) {
     const newAccessToken = await refreshAccessToken();
+
     if (!newAccessToken) {
       localStorage.removeItem("accessToken");
       localStorage.removeItem("refreshToken");
-      window.location.href = "/login"; 
+      
       return response;
     }
 
-    localStorage.setItem("accessToken", newAccessToken);
+    response = await makeRequest(newAccessToken);
   }
 
   return response;
